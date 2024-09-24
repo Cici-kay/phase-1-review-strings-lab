84dc3823c968ecaddf3cdfab844dcaea5207d2e4
Cynthia <kalechecynthia@gmail.com>
2024-09-25 01:40:31 +0300

    Edits file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Cynthia Kaleche';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//console.log(excitedWelcomeMessage)
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+console.log(shortGreeting)
